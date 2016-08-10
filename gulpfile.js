e2b55575319735eeefe968968969a47b6b688fdf
Eric Jackson <ericj1992@gmail.com>
2016-08-10 14:22:23 -0400

    Initial commit
    Pushing project to Github

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+gulp.task('sass', function () {
+  return gulp.src('css/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css'));
+});
+
+
+gulp.task('default', function () {
+  gulp.watch('css/sass/*.scss', ['sass']);
+});
