55426f34f83ea54ca4cbd3f1859e1b52fd348b88
Eric <ericj1992@gmail.com>
2017-03-28 21:40:05 -0400

    Image Compression and Babel Transpiling
    Added gulp tasks for image compression and js transpiling/minification.
    Also moved source files into /src and added /dist folder.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,40 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var babel = require("gulp-babel");
+var minify = require('gulp-minify');
+var imagemin = require('gulp-imagemin');
 
-gulp.task('sass', function () {
-  return gulp.src('css/sass/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('css'))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css'));
+gulp.task("js", function () {
+  return gulp.src("src/js/main.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist/js"))
+    .pipe(minify({
+      ext:{
+        src:'.js',
+        min:'.min.js'
+      }
+    }))
+    // .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/js'));
 });
 
+gulp.task('sass', function () {
+  return gulp.src('src/css/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/css'));
+});
 
-gulp.task('default', function () {
-  gulp.watch('css/sass/**/*.scss', ['sass']);
+gulp.task('img', function() {
+  gulp.src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/css/sass/**/*.scss', ['sass']);
+  gulp.watch('src/js/**/*.js', ['js']);
 });
