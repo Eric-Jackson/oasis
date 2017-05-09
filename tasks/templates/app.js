47f82ffa8c3eb8ec608b88f6bc60d14820cd8c32
Eric <ericj1992@gmail.com>
2017-05-09 10:11:37 -0400

    Began rewriting
    Starting to rework the structure into something more solid.

--- tasks/templates/app.js
+++ tasks/templates/app.js
@@ -1,5 +1,5 @@
 const program = require('commander');
-const app = require('./templates.js');
+const oasis = require('./templates.js');
 
 program
   .version('0.0.1');
@@ -8,35 +8,35 @@ program
   .command('init')
   .description('create template repository')
   .action(function(env, options){
-    app.init();
+    oasis.init();
   });
 
-program
-  .command('reset')
-  .description('overwrite all files by templates from database')
-  .action(function(env, options){
-    app.toFile();
-  });
-
-program
-  .command('upload')
-  .description('overwrite all database templates by templates from files')
-  .action(function(env, options){
-    app.toDb();
-  });
-
-program
-  .command('watch')
-  .description('watch templates')
-  .action(function(env, options){
-    app.watch();
-  });
-
-program
-  .command('delete')
-  .description('delete all file templates')
-  .action(function(env, options){
-    app.clear();
-  });
+// program
+//   .command('reset')
+//   .description('overwrite all files by templates from database')
+//   .action(function(env, options){
+//     app.toFile();
+//   });
+//
+// program
+//   .command('upload')
+//   .description('overwrite all database templates by templates from files')
+//   .action(function(env, options){
+//     app.toDb();
+//   });
+//
+// program
+//   .command('watch')
+//   .description('watch templates')
+//   .action(function(env, options){
+//     app.watch();
+//   });
+//
+// program
+//   .command('delete')
+//   .description('delete all file templates')
+//   .action(function(env, options){
+//     app.clear();
+//   });
 
 program.parse(process.argv);
--- tasks/templates/templates.js
+++ tasks/templates/templates.js
@@ -13,16 +13,14 @@ const config   = require('./config.json'),
       fs       = require('fs'),
       xml2js   = require('xml2js');
 
-module.exports = class Templates {
-    constructor() {
-        this.connection = mysql.createConnection(config.mysql);
-        this.queries = {
-            allTemplates: 'SELECT title, template, sid FROM ' + config.database.prefix + 'templates WHERE sid='+ config.templates.id + ' OR sid=-2',
-            updateTemplates: 'UPDATE '+ config.database.prefix +'templates SET template=\''+ addslashes(data) +'\' WHERE sid='+ config.templates.id +' AND title=\''+ name +'\''
-        };
-    }
+module.exports = {
 
-    init() {
+    connection: mysql.createConnection(config.mysql),
+    queries: {
+        allTemplates: 'SELECT title, template, sid FROM ' + config.database.prefix + 'templates WHERE sid='+ config.templates.id + ' OR sid=-2'
+    },
+
+    init: () => {
         this.connection.connect();
 
         let createDirectory = new Promise((resolve, reject) => {
@@ -45,77 +43,13 @@ module.exports = class Templates {
         });
 
         this.connection.end();
-    }
+    },
 
-    toFile() {
-        this.connection.connect();
+    createTemplate: row => {
+        let title = row.title,
+            name = title.split('_'),
+            dir;
 
-        this.connection.query(this.queries.allTemplates, function(err, rows, fields) {
-            if (err) throw err;
-            for (let i = 0; i < rows.length; i++) {
-                this.createTemplate(rows[i]);
-            }
-        });
-
-        this.connection.end();
-    }
-
-    toDb() {
-        this.connection.connect();
-
-        glob(config.app.datadir + '/**/*' + config.app.fileext, function (err, files) {
-            if (err) throw err;
-            this.refreshDB(files, this.connection);
-        });
-    }
-
-    watch() {
-        this.connection.connect();
-
-        watch.createMonitor(config.app.datadir, function (monitor) {
-            monitor.files = config.app.datadir + '/**/*' + config.app.fileext;
-            console.log('watching');
-            monitor.on("changed", function (f, curr, prev) {
-                this.saveTemplate(f, this.connection);
-            });
-        });
-    }
-
-    // TODO: Make this work
-    loadThemeFile() {
-        let parser = new xml2js.Parser();
-        fs.readFile(__dirname + '/foo.xml', function(err, data) {
-            parser.parseString(data, function (err, result) {
-                console.dir(result);
-                console.log('Done');
-            });
-        });
-    }
-
-    updateDb(name, data, connection) {
-        connection.query(this.queries.updateTemplates, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            console.log('Template '+ name +' changed');
-        });
-    }
-
-    saveTemplate(fullpath, connection) {
-        let filename = path.basename(fullpath, config.app.fileext);
-        fs.readFile(fullpath, 'utf8', function (err,data) {
-            if (err) {
-                return console.log(err);
-            }
-            this.updateDb(filename, data, connection);
-        });
-    }
-
-    createTemplate(row) {
-        let title = row.title;
-        console.log(title);
-        const name = title.split('_');
-        let dir;
         if (this.inArray(title, config.templates.ungrouped)) {
             dir = config.app.datadir +'/ungrouped';
         } else {
@@ -140,32 +74,136 @@ module.exports = class Templates {
                 console.log("Template " + dir + '/' + title + config.app.fileext + " created.");
             });
         });
-    }
+    },
 
-    refreshDB(file, connection) {
-        let sum = file.length;
-        for (let i = 0; i < file.length; i++) {
-            this.saveTemplate(file[i], connection);
-        }
-    }
-
-    addSlashes(string) {
-        return string.replace(/\\/g, '\\\\')
-                     .replace(/\u0008/g, '\\b')
-                     .replace(/\t/g, '\\t')
-                     .replace(/\n/g, '\\n')
-                     .replace(/\f/g, '\\f')
-                     .replace(/\r/g, '\\r')
-                     .replace(/'/g, '\\\'')
-                     .replace(/"/g, '\\"');
-    }
-
-    static inArray(string, array) {
-        return array.indexOf(string) != -1;
-    }
-
-    static clear() {
-        del([config.app.datadir +'/*']);
-        console.log('All data deleted');
-    }
+    inArray: (string, array) => array.indexOf(string) !== -1
 };
+
+// module.exports = class Templates {
+//     constructor() {
+//         this.connection = mysql.createConnection(config.mysql);
+//         this.queries = {
+//             allTemplates: 'SELECT title, template, sid FROM ' + config.database.prefix + 'templates WHERE sid='+ config.templates.id + ' OR sid=-2',
+//             updateTemplates: 'UPDATE '+ config.database.prefix +'templates SET template=\''+ addslashes(data) +'\' WHERE sid='+ config.templates.id +' AND title=\''+ name +'\''
+//         };
+//     }
+//
+//     init() {
+//         this.connection.connect();
+//
+//         let createDirectory = new Promise((resolve, reject) => {
+//             mkdirp(config.app.datadir, error => {
+//                 if (error) {
+//                     console.error(error);
+//                 } else {
+//                     console.log(config.app.datadir + ' directory created.');
+//                 }
+//             });
+//         });
+//
+//         createDirectory.then(() => {
+//             this.connection.query(this.queries.allTemplates, function(err, rows, fields) {
+//                 if (err) throw err;
+//                 for (let i = 0; i < rows.length; i++) {
+//                     this.createTemplate(rows[i]);
+//                 }
+//             });
+//         });
+//
+//         this.connection.end();
+//     }
+//
+
+
+    // toFile() {
+    //     this.connection.connect();
+    //
+    //     this.connection.query(this.queries.allTemplates, function(err, rows, fields) {
+    //         if (err) throw err;
+    //         for (let i = 0; i < rows.length; i++) {
+    //             this.createTemplate(rows[i]);
+    //         }
+    //     });
+    //
+    //     this.connection.end();
+    // }
+    //
+    // toDb() {
+    //     this.connection.connect();
+    //
+    //     glob(config.app.datadir + '/**/*' + config.app.fileext, function (err, files) {
+    //         if (err) throw err;
+    //         this.refreshDB(files, this.connection);
+    //     });
+    // }
+    //
+    // watch() {
+    //     this.connection.connect();
+    //
+    //     watch.createMonitor(config.app.datadir, function (monitor) {
+    //         monitor.files = config.app.datadir + '/**/*' + config.app.fileext;
+    //         console.log('watching');
+    //         monitor.on("changed", function (f, curr, prev) {
+    //             this.saveTemplate(f, this.connection);
+    //         });
+    //     });
+    // }
+    //
+    // // TODO: Make this work
+    // loadThemeFile() {
+    //     let parser = new xml2js.Parser();
+    //     fs.readFile(__dirname + '/foo.xml', function(err, data) {
+    //         parser.parseString(data, function (err, result) {
+    //             console.dir(result);
+    //             console.log('Done');
+    //         });
+    //     });
+    // }
+    //
+    // updateDb(name, data, connection) {
+    //     connection.query(this.queries.updateTemplates, function (err, result) {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         console.log('Template '+ name +' changed');
+    //     });
+    // }
+    //
+    // saveTemplate(fullpath, connection) {
+    //     let filename = path.basename(fullpath, config.app.fileext);
+    //     fs.readFile(fullpath, 'utf8', function (err,data) {
+    //         if (err) {
+    //             return console.log(err);
+    //         }
+    //         this.updateDb(filename, data, connection);
+    //     });
+    // }
+    //
+    //
+    // refreshDB(file, connection) {
+    //     let sum = file.length;
+    //     for (let i = 0; i < file.length; i++) {
+    //         this.saveTemplate(file[i], connection);
+    //     }
+    // }
+    //
+    // addSlashes(string) {
+    //     return string.replace(/\\/g, '\\\\')
+    //                  .replace(/\u0008/g, '\\b')
+    //                  .replace(/\t/g, '\\t')
+    //                  .replace(/\n/g, '\\n')
+    //                  .replace(/\f/g, '\\f')
+    //                  .replace(/\r/g, '\\r')
+    //                  .replace(/'/g, '\\\'')
+    //                  .replace(/"/g, '\\"');
+    // }
+    //
+    // static inArray(string, array) {
+    //     return array.indexOf(string) != -1;
+    // }
+    //
+    // static clear() {
+    //     del([config.app.datadir +'/*']);
+    //     console.log('All data deleted');
+    // }
+//};
