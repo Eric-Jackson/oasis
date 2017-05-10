41e07e7fe3fac6f74d77958118f0332fdf21e664
Eric <ericj1992@gmail.com>
2017-05-10 09:52:18 -0400

    Renamed to Oasis

--- gulpfile.js
+++ gulpfile.js
@@ -9,29 +9,29 @@ var imagemin = require('gulp-imagemin');
 gulp.task("js", function () {
   return gulp.src("src/js/main.js")
     .pipe(babel())
-    .pipe(gulp.dest("dist/themes/clement/js"))
+    .pipe(gulp.dest("dist/themes/oasis/js"))
     .pipe(minify({
       ext:{
         src:'.js',
         min:'.min.js'
       }
     }))
-    .pipe(gulp.dest('dist/themes/clement/js'));
+    .pipe(gulp.dest('dist/themes/oasis/js'));
 });
 
 gulp.task('sass', function () {
   return gulp.src('src/sass/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist/themes/clement/css'))
+    .pipe(gulp.dest('dist/themes/oasis/css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/themes/clement/css'));
+    .pipe(gulp.dest('dist/themes/oasis/css'));
 });
 
 gulp.task('img', function() {
   gulp.src('src/images/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/themes/clement/images'))
+    .pipe(gulp.dest('dist/themes/oasis/images'))
 });
 
 gulp.task('watch', function () {
