75fc7b78b3c368eecc5a37a124ab65fd2cd1b944
Eric <ericj1992@gmail.com>
2017-05-10 20:10:19 -0400

    Updated commands

--- tasks/templates/app.js
+++ tasks/templates/app.js
@@ -5,38 +5,31 @@ program
   .version('0.0.1');
 
 program
-  .command('init')
+  .command('file')
   .description('create template repository')
   .action(function(env, options){
     oasis.init();
   });
 
-// program
-//   .command('reset')
-//   .description('overwrite all files by templates from database')
-//   .action(function(env, options){
-//     app.toFile();
-//   });
-//
-// program
-//   .command('upload')
-//   .description('overwrite all database templates by templates from files')
-//   .action(function(env, options){
-//     app.toDb();
-//   });
-//
-// program
-//   .command('watch')
-//   .description('watch templates')
-//   .action(function(env, options){
-//     app.watch();
-//   });
-//
-// program
-//   .command('delete')
-//   .description('delete all file templates')
-//   .action(function(env, options){
-//     app.clear();
-//   });
+program
+  .command('db')
+  .description('overwrite all database templates by templates from files')
+  .action(function(env, options){
+    oasis.toDb();
+  });
+
+program
+  .command('watch')
+  .description('watch templates')
+  .action(function(env, options){
+    oasis.watch();
+  });
+
+program
+  .command('delete')
+  .description('delete all file templates')
+  .action(function(env, options){
+    oasis.delete();
+  });
 
 program.parse(process.argv);
